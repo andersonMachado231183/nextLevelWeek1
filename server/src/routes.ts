164b4801616501f6bba0e4b82d84201acfc7ea17
andersonMachado231183 <pitt231183@gmail.com>
2020-07-14 20:38:42 -0300

    update image

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,49 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/',(request, response)=>{
-    return response.json({
-        message: 'Hello World'});
+routes.get('/items',async(request, response)=>{ 
+    const items = await knex('items').select('*');
+
+    const serializeItems = items.map(items =>{
+        return {
+            id:items.id,
+            title:items.title,
+            img_url:`http://localhost:3333/uploads/${items.image}`,
+        }
+    });
+
+
+    return response.json({ 
+        serializeItems    
+    });
 });
   
+routes.post('/points', async(request, response)=>{
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+   await knex('points').insert({
+        image:'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,  
+    });
+    return response.json({
+        success:true
+    })
+});
 export default routes;
