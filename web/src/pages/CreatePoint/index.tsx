d9468af772c69f52df000495cc82a63b5201926b
andersonMachado231183 <pitt231183@gmail.com>
2020-08-06 21:54:05 -0300

    ibge uf / cities

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.svg';
 import { FiArrowLeft } from 'react-icons/fi';
@@ -13,22 +13,63 @@ interface Item{
     img_url: string
 }
 
+
+interface IBGEUFResponse{
+    sigla:string
+}
+
+interface IBGECityResponse{
+    nome:string
+}
+
 const CreatePoint = () => {
     const [ items, setItems ] = useState<Item[]>([]);
+    const [ ufs, setUfs ] = useState<string[]>([]);
+    const [ selectedUf, setSelectedUf ] = useState('0');
+    const [ cities, setCities ] = useState<string[]>([]);
+    const [ selectedCity, setSelectedCity ] = useState('0');
 
-
-    useEffect(()=>{
-        axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(
-            response=>{
-                console.log(response);
-            }
-        )
+useEffect(()=>{
+    api.get('items').then(response=>{
+        setItems(response.data);
     })
-    useEffect(()=>{
-        api.get('items').then(response=>{
-            setItems(response.data);
-        })
-    },[]);
+},[]);
+
+useEffect(()=>{
+    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados').then(
+        response=>{
+            const ufInitials = response.data.map(uf=>uf.sigla);
+            setUfs(ufInitials);
+        }
+    )
+},[]);
+
+useEffect(()=>{
+    if(selectedUf === '0'){
+        return;
+    }
+    axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+        .then(response=>{
+            const cityNames = response.data.map(city=>city.nome);
+            setCities(cityNames);
+        }
+    )
+    
+},[selectedUf]);
+
+function handleSelectUf(event: ChangeEvent<HTMLSelectElement>){
+   const uf =  event.target.value;
+
+   setSelectedUf(uf);
+   console.log(uf);
+}
+
+function handleSelectCity(event: ChangeEvent<HTMLSelectElement>){
+   const city =  event.target.value;
+
+   setSelectedCity(city);
+   console.log(city);
+}
 
     return (
         <div id="page-create-point">
@@ -92,14 +133,29 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                              <label htmlFor="uf">Estado ( UF )</label>
-                            <select name="uf" id="uf">
-                            <option value="0">Selecione uma UF</option>    
+                            <select 
+                                name="uf" 
+                                id="uf"
+                                value={selectedUf}
+                                onChange={handleSelectUf}>
+                            <option value="0">Selecione uma UF</option> 
+                            {ufs.map(uf=>(
+                                <option key={uf} value={uf}>{uf}</option>
+                            ))}   
                             </select>
                         </div>
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city">
-                            <option value="auu">Selecione uma cidade</option>    
+                            <select 
+                                    name="city" 
+                                    id="city"
+                                    value={selectedCity}
+                                    onChange={handleSelectCity}
+                                     >
+                            <option value="0">Selecione uma cidade</option> 
+                            {cities.map(city=>(
+                                <option key={city} value={city}>{city}</option>
+                            ))}   
                             </select>
                         </div>
                     </div>
